111361ac21194befeec1a9c0a4059b7f4414cdd8
Chitosama <lilchitodude51@gmail.com>
2023-01-17 04:05:17 -0800

    added first set of js for despartments

---
+++ db/departments.js
@@ -0,0 +1,32 @@
+const db = require("./connection");
+const inquirer = require("inquirer");
+
+async function viewAllDepartments() {
+    try{
+        const departments = db.query('SELECT * FROM department');
+        return departments;
+    } catch (err){
+        console.log(err)
+    }
+}
+
+async function addDepartment() {
+    try{
+        const departments = viewAllDepartments();
+        const {name} = inquirer.prompt([
+            {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter name of new department'
+        }
+        ]);
+        await db.query(`INSERT INTO department (name) VALUES ('${name}')`);
+        const newDepartment = await viewAllDepartments();
+        return newDepartment
+    }
+    catch (err){
+        console.log(err)
+    }
+}
+
+module.exports{viewAllDepartments, addDepartment};
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const {prompt} = require('inquirer');
 const { default: Choice } = require('inquirer/lib/objects/choice');
 const db = require('./db/connection');
-const viewAllDepartments = require('./db/departments');
-const viewAllEmployees = require('./db/employees');
-const viewAllRoles = require('./db/roles');
+const {viewAllDepartments, addDepartment} = require('./db/departments');
+const {viewAllEmployees, addEmployee} = require('./db/employees');
+const {viewAllRoles, addRole} = require('./db/roles');
 
 const start = async (s) => {
     //used to make sure we are running and user friendly
@@ -18,6 +18,9 @@ const start = async (s) => {
             'View all departments',
             'View all employees',
             'view all roles',
+            'Add department',
+            'Add employee',
+            'Add role',
             'Exit'
         ]
 }]);
@@ -31,9 +34,20 @@ switch(Choice){
         const employees = await viewAllEmployees();
         console.table(employees);
         break;
-    case 'view all roles':
+    case 'View all roles':
         const roles = await viewAllRoles();
         console.table(roles);
+        break;
+    case 'Add department':
+        const newDepartment = await addDepartment();
+        console.table(newDepartment);
+        break;
+    case 'Add employee':
+        const newEmployee = await addEmployee();
+        console.table(newEmployee)
+        break;
+    case 'Add role':
+        const newRole = await addRole;
         break;
     case 'Exit':
         console.log('Thank you! Have a wonderful day!');
